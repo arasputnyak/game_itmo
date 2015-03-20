9b740309f12ae5d9dbf6057f04c67d53aa719f8b
arasputnyak <rasputnyak_n@inbox.ru>
2015-03-21 02:10:35 +0400

    Added 9 files

---
+++ ru/ifmo/rasputnyak/Player.java
@@ -0,0 +1,12 @@
+package ru.ifmo.rasputnyak;
+
+import ru.ifmo.rasputnyak.Field;
+
+/**
+ * Created by Владелец on 28.02.2015.
+ */
+public class Player {
+    public void makeStep(Field field) {
+        field.changeMeaning();
+    }
+}
